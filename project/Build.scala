36febaddd0a2fddff6eda825a19b9cc1d9fb9288
Tom Switzer <thomas.switzer@gmail.com>
2012-06-29 14:51:09 -0400

    Create dist task and runner.

--- project/Build.scala
+++ project/Build.scala
@@ -5,6 +5,7 @@ import AssemblyKeys._
 
 object VcfImpBuild extends Build {
   override lazy val settings = super.settings ++ Seq(
+    organization := "OPGIM, Mount Sinai Hospital",
     name := "VCFImp",
     version := "0.5.0",
     scalaVersion := "2.9.2",
@@ -14,6 +15,32 @@ object VcfImpBuild extends Build {
 
   lazy val vcfimp = Project("vcfimp", file("vcfimp"))
   
-  lazy val vcflatten = Project("vcflatten", file("vcflatten")) settings (assemblySettings: _*) dependsOn (vcfimp)
+  lazy val vcflatten = Project("vcflatten", file("vcflatten")) settings (vcflattenSettings: _*) dependsOn (vcfimp)
+
+  def vcflattenSettings = assemblySettings ++ Dist.distSettings
+
+  object Dist {
+    lazy val dist = TaskKey[Unit]("dist", "Packages up bin files and JARs in a zip.")
+    lazy val distName = SettingKey[String]("dist-name", "Name of the dist zip file without the .zip.")
+    lazy val distZip = SettingKey[File]("dist-zip", "Zip file to save dist to.")
+    lazy val binFiles = SettingKey[Seq[File]]("dist-bin-files", "The bin files to include in bin/.")
+
+    lazy val distSettings = Seq(
+      distName in dist <<= (name, version) { (name, version) => name + "-" + version },
+      distZip in dist <<= (target in dist, distName in dist) { (t, n) => t / (n + ".zip") },
+      binFiles in dist <<= (sourceDirectory in dist) { src => (src / "main"/  "bin" * "*").get },
+      dist <<= (distName in dist, distZip in dist, binFiles in dist, outputPath in assembly) map {
+        (name, zip, bins, jar) => distTask(name, zip, bins, Seq(jar))
+      }
+    )
+
+    private def distTask(name: String, zip: File, bins: Seq[File], jars: Seq[File]) {
+      IO.withTemporaryDirectory { dir =>
+        val files = (bins map { f => f -> (name + "/bin/" + f.getName()) }) ++
+                    (jars map { f => f -> (name + "/lib/" + f.getName()) })
+        IO.zip(files, zip)
+      }
+    }
+  }
 }
 
