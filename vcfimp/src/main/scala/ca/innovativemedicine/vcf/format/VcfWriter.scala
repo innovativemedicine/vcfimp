7235de364c253b181e42d449d83e6093ad839175
Tom Switzer <thomas.switzer@gmail.com>
2012-07-13 15:20:34 -0400

    Write VCFs to disk in separate thread.

--- vcfimp/src/main/scala/ca/innovativemedicine/vcf/format/VcfWriter.scala
+++ vcfimp/src/main/scala/ca/innovativemedicine/vcf/format/VcfWriter.scala
@@ -4,6 +4,8 @@ import ca.innovativemedicine.vcf._
 
 import java.io.{ OutputStream, PrintWriter }
 
+import java.util.{ concurrent => juc }
+
 
 object VcfWriter {
   import Metadata._
@@ -36,24 +38,71 @@ object VcfWriter {
     
     // Write variant calls.
     
-    rows foreach { case (variant, fmts, values) =>
-      val fixed = List(
-          formatChromosome(variant.chromosome),
-          variant.position.toString,
-          variant.ids mkString ",",
-          variant.reference,
-          variant.alternates map formatAlternate mkString ",",
-          variant.filter map formatFilterResult getOrElse ".",
-          variant.info map { case (info, vals) =>
-            info.id.id + "=" + (vals map formatVcfValue mkString ",")
-          } mkString ";",
-          fmts map (_.id.id) mkString ":"
-        ) mkString "\t"
-      val samples = values map (_ map (_ map formatVcfValue mkString ",") mkString ":")
-      
-      writer.println((fixed :: samples) mkString "\t")
-    }
+    val dataWriter = new ConcurrentVcfDataWriter(writer)
+    val t = new Thread(dataWriter)
+    t.start()
+    
+    // Any patches will be applied in this thread, the row is written in t.
+    rows foreach { dataWriter write _ }
+    
+    dataWriter.finish()
+    t.join()
     
     writer.close()
+  }
+}
+
+
+object ConcurrentVcfDataWriter {
+  case object EOF
+  case class WriteRow(row: VcfRow)
+}
+
+
+class ConcurrentVcfDataWriter(writer: PrintWriter) extends Runnable {
+  import Metadata._
+  import VcfFormatter._
+  import ConcurrentVcfDataWriter._
+  
+  @volatile private var _stayAlive = true
+  def kill() { _stayAlive = false }
+  private def stayAlive = _stayAlive || Thread.currentThread().isInterrupted()
+  
+  private val queue: juc.BlockingQueue[Any] = new juc.ArrayBlockingQueue[Any](100)
+  
+  def write(row: VcfRow) {
+    queue.put(WriteRow(row))
+  }
+  
+  def finish() {
+    queue.put(EOF)
+  }
+  
+  def run() {
+    while (stayAlive) {
+      Option(queue.poll(10, juc.TimeUnit.MILLISECONDS)) match {
+        case Some(WriteRow((variant, fmts, values))) =>
+          val fixed = List(
+              formatChromosome(variant.chromosome),
+              variant.position.toString,
+              variant.ids mkString ",",
+              variant.reference,
+              variant.alternates map formatAlternate mkString ",",
+              variant.filter map formatFilterResult getOrElse ".",
+              variant.info map { case (info, vals) =>
+                info.id.id + "=" + (vals map formatVcfValue mkString ",")
+              } mkString ";",
+              fmts map (_.id.id) mkString ":"
+            ) mkString "\t"
+          val samples = values map (_ map (_ map formatVcfValue mkString ",") mkString ":")
+          
+          writer.println((fixed :: samples) mkString "\t")
+          
+        case Some(EOF) =>
+          _stayAlive = false
+        
+        case Some(_) | None =>
+      }
+    }
   }
 }
